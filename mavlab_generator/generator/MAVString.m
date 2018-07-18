b56339885a291b32dad74afc15639cabc595fddb
UoBDASAR <40834585+UoBDASAR@users.noreply.github.com>
2018-07-18 15:33:47 +0100

    Add files via upload

---
+++ mavlab_generator/generator/MAVString.m
@@ -0,0 +1,142 @@
+function [outString,exitChar] = MAVString(inString,schema)
+%MAVSTRING: The MAVLAB templating engine
+%Description:
+%    Parses a string and injects data from a schema into it using a simple templating system. Error
+%    checking is minimal so use with caution.
+%Arguments:
+%    inString: The string to be used as the template
+%    schema: The structure from which data will be injected into the template
+%    startIndex: Index at which to start parsing the template string
+%    endIndex: Index at which to stop parsing the template string
+%Usage:
+%    Variable: ${var}$
+%    Repetition: #{array<#>text}#
+%    Conditional: ?{condition<?>expression1<?>expression2}?
+
+    %Initialise some variables
+    varName = []; condition = []; outString = []; exitChar = 0;
+    conditionStart = 0;
+    
+    %Find all tags in the file and sort them in character index order
+    tags = sort([strfind(inString,'${') strfind(inString,'#{') strfind(inString,'?{') strfind(inString,'}#') ...
+        strfind(inString,'}?') strfind(inString,'<?>')]);
+    
+    %Initialise the parser loop
+    stringIndex = 1;
+    
+    %Parse until no more special tags are found
+    for tagIndex=1:1:size(tags,2)
+        %Ignore any tags that occur before the current string index
+        if tags(tagIndex) >= stringIndex;
+            %Add text between the current string index and next tag to the output string
+            outString = [outString inString(stringIndex:tags(tagIndex)-1)];
+            %Determine the tag type and handle appropriately
+            switch(inString(tags(tagIndex):tags(tagIndex)+1))
+                %Variable start tag
+                case '${'
+                    [returnedString,exitChar] = variable(inString(tags(tagIndex)+2:end),schema);
+                    outString = [outString, returnedString];
+                    stringIndex = tags(tagIndex) + exitChar + 1;
+                %Repetition start tag
+                case '#{'
+                    [returnedString,exitChar] = repetition(inString(tags(tagIndex)+2:end),schema);
+                    outString = [outString, returnedString];
+                    stringIndex = tags(tagIndex) + exitChar + 1;
+                %Conditional start tag
+                case '?{'
+                    [returnedString,exitChar] = conditional(inString(tags(tagIndex)+2:end),schema);
+                    outString = [outString, returnedString];
+                    stringIndex = tags(tagIndex) + exitChar + 1;
+                %Repetition end tag
+                case '}#'
+                    exitChar = tags(tagIndex) + 1;
+                    return;
+                %Conditional end tag
+                case '}?'
+                    exitChar = tags(tagIndex) + 1;
+                    return;
+                %Handles the 3 character conditional delimiter
+                otherwise
+                    exitChar = tags(tagIndex) + 2;
+                    return;
+            end
+            
+        end
+        
+    end
+    %Add remaining text after the last found tag to the output string
+    outString = [outString inString(stringIndex:end)];
+end
+
+function [outString,exitChar] = variable(inString,schema)
+%VARIABLE: Handles replacement of ${var}$ tags
+%Description:
+%    Replaces a variable tag with data found in the supplied schema. Throws an error if the input
+%    string is not formatted correctly.
+%Arguments:
+%    inString(string): String to be parsed for a variable name
+%    schema(struct): Source of the data used to replace the ${var}$ tag
+    outString = [];
+    tagStart = strfind(inString,'}$');
+    varName = inString(1:tagStart(1)-1);
+    exitChar = tagStart(1)+2;
+    if ~isfield(schema,varName)
+        disp('ERROR: Field does not exist!');
+    elseif isnumeric(schema.(varName))
+        outString = num2str(schema.(varName));
+    else
+        outString = schema.(varName);
+    end
+end
+
+function [outString,exitChar] = repetition(inString,schema)
+%VARIABLE: Handles replacement of #{array<#>text}# tags
+%Description:
+%    Replaces a repetition tag with data found in the supplied schema. Throws an error if the input
+%    string is not formatted correctly. Supports nesting.
+%Arguments:
+%    inString(string): String to be parsed for an array name and replacement text
+%    schema(struct): Source of the array data
+    outString = [];
+    tagStart = strfind(inString,'<#>');
+    varName = inString(1:tagStart(1)-1);
+    if ~isfield(schema,varName)
+        disp('ERROR: Field does not exist!');
+        outString = [];
+        exitChar = [];
+        return;
+    else
+        newSchema = schema.(varName);
+        for j=1:1:size(newSchema,2)
+            [returnedString,exitChar] = MAVString(inString(tagStart(1)+3:end),newSchema(j));
+            outString = [outString returnedString];
+            exitChar = exitChar + tagStart(1) + 3;
+        end
+    end
+end
+
+function [outString,exitChar] = conditional(inString,schema)
+%VARIABLE: Handles replacement of ?{condition<?>expression1<?>expression2}? tags
+%Description:
+%    Replaces a conditional tag with data found in the supplied schema. Throws an error if the input
+%    string is not formatted correctly. Does not support nesting.
+%Arguments:
+%    inString(string): String to be parsed for a condition and expressions
+%    schema(struct): Source of the data used by any nested tags
+    outString = []; evaluation = -1;
+    tagStart = strfind(inString,'<?>');
+    [condition,~] = MAVString(inString(1:tagStart(1)-1),schema);
+    try
+        eval(sprintf('if %s,evaluation=1;,else,evaluation=0;,end',condition));
+    catch
+        disp('ERROR: Condition is not valid!');
+    end
+    if evaluation == 1
+        [outString,~] = MAVString(inString(tagStart(1)+3:end),schema);
+    elseif evaluation == 0
+        [outString,~] = MAVString(inString(tagStart(2)+3:end),schema);
+    end
+    tagStart = strfind(inString,'}?');
+    exitChar = tagStart(1) + 2;
+end
+
---
+++ mavlab_generator/generator/MAVStruct.m
@@ -0,0 +1,218 @@
+classdef MAVStruct
+%MAVSTRUCT: The MAVLAB XML toolbox
+%Description:
+%    Reads XML files and parses the nodes into a structure. Includes convenience functions that
+%    allow searching for fields of a specified name.
+    
+    properties
+        name;           %Name of this node
+        text;           %Text field of the node
+        children;       %Array of child MAVStructs
+        attributes;             %Attributes of this node
+        hasChildren = 0;        %Does the children field exist
+        hasText = 0;            %Does the text field exist
+        hasAttributes = 0;      %Does the attributes field exist
+    end
+    
+    methods
+        
+        function obj = MAVStruct(filename,name,text,children,attributes, hasText, hasChildren, hasAttributes)
+        %MAVSTRUCT: Generates a searchable structure from XML
+        %Description:
+        %    The constructor for creating a new MAVStruct object from an XML file.
+        %Arguments:
+        %    filename(string): Path to the XML file to be parsed
+        %    name(string): Name of this MAVStruct
+        %    text(text): Text content of this MAVStruct
+        %    children(MAVStruct): Array of child MAVStructs
+        %    attributes(struct): Array of attribute structs
+            
+            %If there is 1 input argument create a MAVStruct from an XML file
+            if nargin == 1
+                
+                %Display console message and start timer
+                disp(['Parsing XML file: ' filename]);
+                timer = tic();
+                %Read the DOM tree for this XML file
+                try
+                   tree = xmlread(filename);
+                catch
+                   error('Failed to read XML file %s.',filename);
+                end
+                %Create a structure by parsing the DOM tree
+                [children,~,~,~] = MAVStruct.parseChildNodes(tree);
+                obj = MAVStruct([],'root','This is the root of the XML file',children,[],1,1,0);
+                %Display time taken to parse
+                disp(['Parsing completed in: ' num2str(toc(timer)) ' seconds']);
+                
+            %Otherwise if there are 8 arguments create a new MAVStruct using fields
+            elseif nargin == 8
+                obj.name = name;
+                obj.text = text;
+                obj.children = children;
+                obj.attributes = attributes;
+                obj.hasText = hasText;
+                obj.hasChildren = hasChildren;
+                obj.hasAttributes = hasAttributes;
+            end
+            
+        end
+        
+        function result = find(obj,query)
+        %FIND: Finds the first child with the requested name
+        %Description:
+        %    Searches the current nodes children array and attempts to find the first child with the name
+        %    requested.
+        %Arguments:
+        %    query(string): The name of the child being requested
+            
+            result = {};
+            if ~isempty(obj.children)
+                for i = 1:1:size(obj.children,2)
+                    if strcmp(obj.children{i}.name,query)
+                        result = obj.children{i};
+                        break;
+                    end
+                end
+            end
+            
+        end
+        
+        function result = findAll(obj,query)
+        %FINDALL: Finds all children with the requested name
+        %Description:
+        %    Searches the current nodes children array and attemts to find all of the children with the name
+        %    requested.
+        %Arguments:
+        %    query(string): The name of the children being requested
+            
+            result = {};
+            if ~isempty(obj.children)
+                for i = 1:1:size(obj.children,2)
+                    if strcmp(obj.children{i}.name,query)
+                        result = [result obj.children{i}];
+                    end
+                end
+            end
+            
+        end
+        
+        function result = findAllUntil(obj,query,until)
+        %FINDALLUNTIL: Finds all children with the requested name until a given other child is found
+        %Description:
+        %    Searches the current nodes children array and attemts to find all of the children with the name
+        %    requested until a given field is found.
+        %Arguments:
+        %    query(string): The name of the children being requested
+        %    until(string): The name of the cut-off child
+            
+            result = {};
+            if ~isempty(obj.children)
+                
+                for i = 1:1:size(obj.children,2)
+                    if strcmp(obj.children{i}.name,query)
+                        result = [result obj.children{i}];
+                    elseif strcmp(obj.children{i}.name,until)
+                        break;
+                    end
+                end
+                
+            end
+            
+        end
+        
+    end
+    
+    methods(Static, Access = private)
+        
+        function [children, text,hasText,hasChildren] = parseChildNodes(theNode)
+        %PARSECHILDNODES: Parses the children of the current node
+        %Description:
+        %    Recursively finds the children of each node in a DOM tree starting from the specified node.
+        %Arguments:
+        %    theNode(DOM): The XML tree obtained from xmlread() which needs to be parsed
+
+            children = {};
+            text = [];
+            hasText = 0;
+            hasChildren = 0;
+            
+            %If the current node has children process them and add to the child array
+            if hasChildNodes(theNode)
+                childNodes = getChildNodes(theNode);
+                numChildNodes = getLength(childNodes);
+
+                %Iterate and parse each child node
+                count = 1; i = 1;
+                while i <= numChildNodes
+                    theChild = childNodes.item(i-1);
+                    %If the child node name is '#text' and not whitespace, found text field of the parent node
+                    newChild = MAVStruct.makeStructFromNode(theChild);
+                    if strcmp(newChild.name, '#text')
+                        newText = strtrim(char(getTextContent(theChild)));
+                        if newChild.hasChildren == 0 && ~strcmp(newText,'')
+                            text = newText;
+                            hasText = 1;
+                        end 
+                    %Otherwise, create a MAVStruct from the child and add to the child array.
+                    else
+                        hasChildren = 1;
+                        children{count} = newChild;
+                        count = count + 1;
+                    end
+                    i = i + 1;
+                end
+            end
+        end
+        
+        function newStruct = makeStructFromNode(theNode)
+        %MAKESTRUCTFROMNODE: Generates a structure from the current node
+        %Description:
+        %    Reads all of the data from the current node and returns it as a new MAVStruct
+        %Arguments:
+        %    theNode(DOM): The XML node to be parsed into a structure
+            
+            %Obtain children and attributes of this node
+            [children, text, hasText, hasChildren] = MAVStruct.parseChildNodes(theNode);
+            [attributes, hasAttributes] = MAVStruct.parseAttributes(theNode);
+            %Set the name of this node
+            name = char(getNodeName(theNode));
+            
+            newStruct = MAVStruct([],name,text,children,attributes,hasText,hasChildren,hasAttributes);
+
+        end
+
+        function [attributes, hasAttribs] = parseAttributes(theNode)
+        %PARSEATTRIBUTES: Creates a structure array from the attributes of the specified node
+        %Description:
+        %    Finds all of the attributes of the current node and stores each as a structure in an array.
+        %Arguments:
+        %    theNode(DOM): The XML node from which to extract attributes
+
+            attribFields = {};
+            attribValues = {};
+
+            %If the node has attributes, add each to the attribute array
+            if hasAttributes(theNode)
+               hasAttribs = 1;
+               theAttributes = getAttributes(theNode);
+               numAttributes = getLength(theAttributes);
+
+               %Aquire the name and value of the current attribute
+               for i = 1:1:numAttributes
+                   attrib = theAttributes.item(i-1);
+                   attribFields = [attribFields {char(getName(attrib))}];
+                   attribValues = [attribValues {char(getValue(attrib))}];
+               end
+            else
+                hasAttribs = 0;
+            end
+
+            %Convert the attributes cell array to a struct array
+            attributes = cell2struct(attribValues,attribFields,2);
+        end
+        
+    end
+    
+end
+
